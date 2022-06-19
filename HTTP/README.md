c82ab13e40dbaa8b57ca540e57e446f2c05878df
lyc8503 <lyc8503@foxmail.com>
2022-06-19 15:15:26 +0800

    refactor

--- HTTP/README.md
+++ HTTP/README.md
@@ -15,7 +15,20 @@ pip3 install -r requirements.txt
 python3 setup.py --help
 ```
 
+## 阿里云一键部署
+
+需要阿里云账号 ID, AccessKeyID, AccessKeySecret, 可登录账号后自行申请.
+
+使用本项目前请先手动开通函数计算 FC 服务, 同时不要忘记给子账号授权 FC 相关权限.
+
+运行如下代码查看具体部署方式
+
+```bash
+python3 setup_aliyunfc.py --help
+```
+
 ## 手动项目配置
+
 ### 函数配置
 1. 开通[腾讯云函数服务](https://console.cloud.tencent.com/scf/list)
 2. 在 函数服务 > 新建 中使用自定义创建，函数名称及地域任选，运行环境选择 Python3.6。
--- HTTP/src/server_aliyunfc.py
+++ HTTP/src/server_aliyunfc.py
@@ -1,24 +1,10 @@
 # -*- coding: utf8 -*-
 import json
-import pickle
+import urllib3
 from base64 import b64decode, b64encode
-
-import requests
-
-
-SCF_TOKEN = "Token"
 
 
 def handler(environ: dict, start_response):
-    try:
-        token = environ["HTTP_SCF_TOKEN"]
-        assert token == SCF_TOKEN, "Invalid token."
-    except:
-        status = '403 Forbidden'
-        response_headers = [('Content-type', 'text/json')]
-        start_response(status, response_headers)
-        return []
-
     try:
         request_body_size = int(environ.get('CONTENT_LENGTH', 0))
     except (ValueError):
@@ -26,16 +12,19 @@ def handler(environ: dict, start_response):
     request_body = environ['wsgi.input'].read(request_body_size)
 
     kwargs = json.loads(request_body.decode("utf-8"))
-    kwargs['data'] = b64decode(kwargs['data'])
-    # Prohibit automatic redirect to avoid network errors such as connection reset
-    r = requests.request(**kwargs, verify=False, allow_redirects=False)
+    kwargs['body'] = b64decode(kwargs['body'])
 
-    # TODO: REFACTOR NEEDED. Return response headers and body directly.
-    # There are many errors occured when setting headers to r.headers with some aujustments(https://cloud.tencent.com/document/product/583/12513).
-    # and the response `r.content`/`r.raw.read()` to body.(like gzip error)
-    serialized_resp = pickle.dumps(r)
+    http = urllib3.PoolManager()
+    # Prohibit automatic redirect to avoid network errors such as connection reset
+    r = http.request(**kwargs, retries=False, decode_content=False)
+
+    response = {
+        "headers": {k.lower(): v.lower() for k, v in r.headers.items()},
+        "status_code": r.status,
+        "content": b64encode(r._body).decode('utf-8')
+    }
 
     status = '200 OK'
     response_headers = [('Content-type', 'text/json')]
     start_response(status, response_headers)
-    return [b64encode(serialized_resp)]
+    return [json.dumps(response)]
